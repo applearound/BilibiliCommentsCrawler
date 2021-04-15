f287649017a9a5c7599f52bb5170927a61f5a277
yz <yz@lightedpath.local>
2021-04-16 01:03:04 +0800

    更新API的实现

--- crawler/__main__.py
+++ crawler/__main__.py
@@ -16,7 +16,8 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
     datefmt='%a, %d %b %Y %H:%M:%S',
-    stream=sys.stdout)
+    stream=sys.stdout
+)
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument(
